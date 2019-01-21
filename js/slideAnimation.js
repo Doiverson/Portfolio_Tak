90d800a7ae86e86f2d486ead6021cace9922f66c
Shosuke Doi <doiverson.canada@gmail.com>
2019-01-21 14:30:54 -0800

    add hover effect on sns icons

--- js/slideAnimation.js
+++ js/slideAnimation.js
@@ -1,3 +1,3 @@
 
 const slideElement = document.getElementsByClassName('slider-element');
-TweenMax.to(slideElement, 2, {opacity:1, x:0 , ease:Sine.easeOut});
+TweenMax.to(slideElement, 2, {opacity:1, x:0 , ease:Sine.easeOut, delay:0.2});
