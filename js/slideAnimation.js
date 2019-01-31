795224028243ac7b34a9901bdae97a61ee6a9d4d
Shosuke Doi <doiverson.canada@gmail.com>
2019-01-30 18:35:04 -0800

    completed all

--- js/slideAnimation.js
+++ js/slideAnimation.js
@@ -1,3 +1,6 @@
 
 const slideElement = document.getElementsByClassName('slider-element');
-TweenMax.to(slideElement, 2, {opacity:1, x:0 , ease:Sine.easeOut, delay:0.2});
+TweenMax.to(slideElement, 2, {opacity:1, x:0 , ease:Sine.easeOut, delay:0.5});
+
+const slideIamge = document.getElementsByClassName('slider-image');
+TweenMax.to(slideIamge, 2, {opacity:1, x:0 , ease:Sine.easeOut, delay:0.2});
